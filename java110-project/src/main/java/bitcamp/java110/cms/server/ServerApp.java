489200b32f809d488b723c2999a4d28777b2c11d
kim <chfaos120@naver.com>
2018-09-14 17:06:41 +0900

    xxxx/list 요청처리

--- java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
+++ java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
@@ -7,29 +7,56 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bitcamp.java110.cms.context.RequestMappingHandlerMapping;
+import bitcamp.java110.cms.context.RequestMappingHandlerMapping.RequestMappingHandler;
 
 public class ServerApp {
 
     static Scanner keyIn = new Scanner(System.in);
+    ClassPathXmlApplicationContext iocContainer;
+    RequestMappingHandlerMapping requestHandlerMap;
+    public ServerApp() throws Exception {
+        createIoCContainer();
+        logBeanOfContainer();
+        processRequestHandlerMappingAnnotation();
+    }
+    
+    private void createIoCContainer() {
+        iocContainer = new ClassPathXmlApplicationContext("bitcamp/java110/cms/conf/application-context.xml");
+    }
+    
+    private void processRequestHandlerMappingAnnotation() {
+        requestHandlerMap = new RequestMappingHandlerMapping();
+        String[] names = iocContainer.getBeanDefinitionNames();
+        for (String name : names) {
+            Object obj = iocContainer.getBean(name);
 
-    public static void main(String[] args) throws Exception {
-
-        //클라이언트 연결을 기다리는 서버 소켓 준비
+            requestHandlerMap.addMapping(obj);
+        }
+    }
+    private void logBeanOfContainer() {
+        System.out.println("--------------------------");
+        String [] nameList = iocContainer.getBeanDefinitionNames();
+        for(String name : nameList) {
+            System.out.println(name);
+        }
+        System.out.println("--------------------------");
+    }
+    
+    public void service() throws Exception{
         ServerSocket serverSocket = new ServerSocket(8888); 
         System.out.println("서버 실행 중 . . . . . . . . . . . .");
-        //backlog = 대기열 크기  , 대기열 크기를 벗어나면 거절당함
-        //queue방식으로 관리함 : 먼저 오는 순서대로 관리하는 것
-
+        
         while(true) {
             try (
                     Socket socket = serverSocket.accept();
-
                     PrintStream out =new PrintStream(new BufferedOutputStream(socket.getOutputStream()));
-
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     ) {
                 System.out.println(in.readLine());
-                out.println("ok"); out.flush();
+                out.println("ok : kyr"); out.flush();
                 while(true) {
                     String requestLine = in.readLine();
                     if(requestLine.toUpperCase().equals("EXIT")) {
@@ -38,11 +65,30 @@ public class ServerApp {
                         out.flush();
                         break;
                     }
-                    out.println(requestLine);//출력
+                    RequestMappingHandler mapping = requestHandlerMap.getMapping(requestLine);
+                    if (mapping == null) {
+                        out.println("해당 요청을 처리할 수 없습니다.");
+                        out.println();
+                        out.flush();
+                        continue;
+                    }
+                    
+                    try {
+                        mapping.getMethod().invoke(mapping.getInstance(), out);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        out.println("요청 처리 중에 오류가 발생하였습니다.");
+                    }
                     out.println(); // ("")이렇게 해도상관없음.
                     out.flush();
                 }
             }
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        ServerApp serverApp = new ServerApp();
+        serverApp.service();
     }
 }
