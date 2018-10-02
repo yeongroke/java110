4f6eb2b054d04a780bae24e0c7ccb5540d609410
kim <chfaos120@naver.com>
2018-10-02 11:45:50 +0900

    logout화면 완성

--- java110-project/src/main/java/bitcamp/java110/cms/servlet/HeaderServlet.java
+++ java110-project/src/main/java/bitcamp/java110/cms/servlet/HeaderServlet.java
@@ -2,12 +2,16 @@ package bitcamp.java110.cms.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.ibatis.javassist.expr.Instanceof;
+import bitcamp.java110.cms.domain.Manager;
+import bitcamp.java110.cms.domain.Member;
+import bitcamp.java110.cms.domain.Teacher;
 
 @WebServlet("/header")
 public class HeaderServlet extends HttpServlet {
@@ -22,8 +26,23 @@ public class HeaderServlet extends HttpServlet {
         
         out.println("<header>");
         out.println("    <h1>비트캠프</h1>");
-            
         out.println("    <ul>");
+        
+        HttpSession session=request.getSession();
+        Member loginUser = (Member)session.getAttribute("loginUser");
+        
+        if(loginUser == null) {
+            out.println("<li><a href='/auth/login'>로그인</a><l/i>");
+        } else {
+            String loginType = "학생";
+            if (loginUser instanceof Manager) {
+                loginType ="매니저";
+            } else if(loginUser instanceof Teacher) {
+                loginType ="강사";
+            }
+            out.printf("<li>[%s]%s(<a href='/auth/logout'>로그아웃</a>)</li>\n",loginType,loginUser.getName());
+        }
+        
         out.println("        <li><a href='/student/list'>학생관리</a></li>");
         out.println("        <li><a href='/teacher/list'>강사관리</a></li>");
         out.println("        <li><a href='/manager/list'>매니저관리</a></li>");
---
+++ java110-servlet/src/main/java/bitcamp/java110/ex11/Servlet01.java
@@ -0,0 +1,40 @@
+package bitcamp.java110.ex11;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//세션 사용 전 - hidden 
+@WebServlet("/ex11/servlet01")
+public class Servlet01 extends HttpServlet{
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset='UTF-8'>");
+        out.println("<title>non-session</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>1페이지</h1>");
+        out.println("<form action='servlet02' method= 'post'>");
+        out.println(" 이름: <input type = 'test' name= 'name'><br>");
+        out.println("<button>다음</button>");
+        out.println("</form>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
